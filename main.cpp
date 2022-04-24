38849455a8e1b347bdd6a20c843b2ab3c37eb8dd
Matheus Victor <matheusvictor.salles@gmail.com>
2022-04-23 21:41:55 -0300

    Testa chamadas de funções no main

--- main.cpp
+++ main.cpp
@@ -1,59 +1,33 @@
-#include <limits>
-#include <vector>
-#include <cstdlib>
-#include <iostream>
-#include <algorithm>
-
+#include "geraVetores.h"
 #include "buscaSequencial.h"
-
-using namespace std;
-
-
-#define TAMANHO_MAXIMO 20
 
 
 int main() {
+
+    // unsigned int chaveBusca = (rand() % 100);
+    unsigned int chaveBusca = 41;
+
+    vector<int> vetorDesordenado = gerarVetorDesordenado();
+    vector<int> vetorOrdenado = ordenarVetor(vetorDesordenado);
+
+    // imprimir os dois vetores
+    cout << "Vetor desordenado: ";
+    for (auto x : vetorDesordenado)
+        cout << x << " ";
+
+    cout << "\n";
+
+    cout << "Vetor ordenado: ";
+    for (auto x : vetorOrdenado)
+        cout << x << " ";
     
-    vector<int> vetor;
+    cout << "\n";
+
+    cout << "chave: " << chaveBusca << endl;
+
+    cout << "no ordenado:" << buscaSequencial(vetorOrdenado, chaveBusca) << endl;
+    cout << "no desordenado:" << buscaSequencial(vetorDesordenado, chaveBusca) << endl;
     
-    cout << "Tamanho do vetor: " << vetor.size() << endl;
-    srand(time(NULL));
-
-    cout << " ************************************************ " << endl;
-    cout << "Preenchendo o vetor com valores aleatorios..." << endl;
-    for (unsigned int i=0; i < TAMANHO_MAXIMO; i++) {
-        vetor.push_back(rand() % 100); // números randômicos de 0 a 100
-    }
-    cout << " ************************************************ " << endl;
-
-    cout << " ************************************************ " << endl;
-    cout << "Tamanho do vetor depois de preenchido: " << vetor.size() << endl;
-    cout << " ************************************************ " << endl;
-
-    cout << "Conteudo do vetor: ";
-    for (unsigned int i=0; i < TAMANHO_MAXIMO; i++) {
-        cout << vetor[i] << " ";
-    }
-    cout << endl << " ************************************************ " << endl;    
-    
-    cout << "Ordenando o vetor... ";
-    sort(vetor.begin(), vetor.end());
- 
-    cout << "Vetor ordenado: " << endl;
-    for (auto x : vetor) cout << x << " ";
-    cout << endl << " ************************************************ " << endl;    
-
-
-    for (unsigned int i=0; i < TAMANHO_MAXIMO / 2; i++) {
-        int chaveAleatoria = rand() % 100;
-
-        if (buscaSequencial(vetor, chaveAleatoria)){
-            cout << "Achei a chave " << chaveAleatoria << endl;
-        } else{
-            cout << "NAO ENCONTREI a chave " << chaveAleatoria << endl;
-        }
-    }
-
     system("pause");
 
     return 0;
